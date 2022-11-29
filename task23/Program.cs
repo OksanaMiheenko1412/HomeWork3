8c78cab5d51bcdb13e8c8c7dea7345700d1a1956
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-29 22:39:47 +0700

    / add  new code task23

--- task23/Program.cs
+++ task23/Program.cs
@@ -4,4 +4,4 @@ Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= n; i++)
-    Console.WriteLine($"{i * i * i}");
+    Console.WriteLine(Math.Pow(i, 3));
