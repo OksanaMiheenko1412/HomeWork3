08054fbaaebfba529c00a3a33651fcf3a77a0574
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-29 22:10:22 +0700

    / add commit task21

--- task21/Program.cs
+++ task21/Program.cs
@@ -13,3 +13,7 @@ double y2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите координату Z2: ");
 double z2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)));
+// Match - класс(объект),в котором  есть определенный метод;
+// Sqrt - метод извлечения квадратного корня; 
+// Pow - метод возведения в степеньж
+//  2 - степень, в которую возводим
