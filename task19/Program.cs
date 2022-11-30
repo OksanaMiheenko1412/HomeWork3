a8d8636c18192682a9f132cb31dc3b6aa420d696
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-30 21:00:48 +0700

    / add   code task19

---
+++ task19/Program.cs
@@ -0,0 +1,11 @@
+﻿// task19
+
+Console.Clear();
+int n = new Random().Next(10000, 99999); // [10000, 99999]
+Console.WriteLine($"Случайное число: {n}");
+int n2 = n / 1000; // первые две цифры
+int n4 = n % 100; // последние две цифры
+if(n2 == n4)
+    Console.Write("yes");
+else
+    Console.Write("no");    
