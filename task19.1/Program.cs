aaf9a6ed1a565b317e2fb47dedea1fa8baf8d0e0
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-02 11:46:13 +0700

    / add new code commit task19.1

--- task19.1/Program.cs
+++ task19.1/Program.cs
@@ -1,16 +1,17 @@
-﻿// task19: на вход (пятизначное число) на выход (является ли число палиндромом)
+﻿// task19: на вход любое число на выход (является ли число палиндромом)
 
 Console.Clear();
 
 Console.WriteLine("Введите пятизнвчное число ");
-int a = Convert.ToInt32(Console.ReadLine());
-int result = a;
-int newNumber = 0, i = Convert.ToString(a).Length -1;
-
-while(i >0)
+int n = Convert.ToInt32(Console.ReadLine());
+int result = n;
+int newNumber = 0, i = Convert.ToString(n).Length -1; // перевод числа в строку
+// нахождение длины строки, т.е. количество символов  внутри строки
+while(i >= 0)
 {
-    newNumber += (a % 10 * Convert.ToInt32(Math.Pow(10, i)));
-}
     
-Console.WriteLine(newNumber);
-Console.WriteLine(newNumber == a);   
+        newNumber +=  newNumber + (n % 10 * Convert.ToInt32(Math.Pow(10, i))); 
+        n = n / 10;
+        i--;
+}    
+Console.WriteLine(newNumber == result);   
